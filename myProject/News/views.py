7941f4e2df116c4f69df8e238401742f8f5c36cc
rakeshshraddha <156996517+rakeshshraddha@users.noreply.github.com>
2024-01-23 13:46:07 +0530

    dt

--- myProject/News/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def News(request):
-    return HttpResponse('Hello')
--- myProject/home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from home.models import About
-
-# Register your models here.
-admin.site.register(About)
